259d24da000b6373dd3b9730a8f083aa734a4db9
Emma Romero <emmayrs17@gmail.com>
2021-09-19 18:51:28 -0500

    first commit

---
+++ redCSV.py
@@ -0,0 +1,11 @@
+import csv
+
+with open('valores.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    Xfile=[]
+    Yfile=[]
+    for line in csv_reader:
+        Xfile.append(line['x'])
+        Yfile.append(line['y'])
+    print(Xfile)
+    print(Yfile)
