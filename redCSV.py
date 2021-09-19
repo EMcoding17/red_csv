92ec6dfeb5b628e43e53cb9dcdc6783fcdd59393
Emma Romero <emmayrs17@gmail.com>
2021-09-19 18:54:40 -0500

    obtencion de csv correcto

--- redCSV.py
+++ redCSV.py
@@ -4,6 +4,7 @@ with open('valores.csv', 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     Xfile=[]
     Yfile=[]
+    
     for line in csv_reader:
         Xfile.append(line['x'])
         Yfile.append(line['y'])
